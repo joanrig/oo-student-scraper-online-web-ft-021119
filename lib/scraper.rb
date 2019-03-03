2210aea89ef987719664b3d1f0c77215d6658181
Joan Lyness <joanrigdon@gmail.com>
2019-03-03 11:03:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,37 +2,63 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  attr_accessor :students
+
 
   def self.scrape_index_page(index_url)
-      html = open('./fixtures/student-site/index.html')
-      doc = Nokogiri::HTML(html)
-      students =[]
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    @@students =[]
 
-      doc.css("div.roster-cards-container").each do |card|
-        card.css(".student-card a").each do |info|
-        binding.pry
-        #get name and city
-        # doc.css("div.card-text-container").each do |info|
-        #   @name = info.children.text.split[0..1].join(" ")
-        #   @location = info.children.text.split[2..3].join(" ").delete(",")
-            #binding.pry
-        end
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |stu|
+        name = stu.css(".student-name").text
+        location = stu.css(".student-location").text
+        profile_url = "#{stu.attr("href")}"
+        student = {name: name, location: location, profile_url: profile_url}
+        @@students << student
       end
-
-        #get profile link
-        link_end = student_info.css("div.student-card").xpath('//div/a/@href').first.value
-        @profile = "./fixtures/student-site/#{link_end}"
-        binding.pry
-
-        #populate student hash, save to student array
-        student = {:name => @name, :location => @location, :profile_url => @profile}
-        student_array << student
-        #binding.pry
-
     end
+    @@students
+  end
+
+
+
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open profile_url)
+    student = {}
+    students = []
+
+    social = doc.css(".social-icon-container a").map {|icon| icon.attribute("href").value}
+    social.each do |link|
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
+      #binding.pry
+    end
+
+    doc.css(".profile-quote").each do |quote|
+      student[:profile_quote] = quote.text
+    end
+
+    #binding.pry
+
+    bio_info = doc.css(".description-holder p").each do |bio|
+      student[:bio] = bio.text
+    end
+
+    students << student
+    students[0]
 
   end
+
+
 
 end
