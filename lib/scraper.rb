074a6949a9cb61a96565f8b8890974193fc7f676
Joan Lyness <joanrigdon@gmail.com>
2019-03-03 09:17:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,20 +4,32 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open('./fixtures/student-site/index.html')
-    doc = Nokogiri::HTML(html)
+      html = open('./fixtures/student-site/index.html')
+      doc = Nokogiri::HTML(html)
+      students =[]
 
-    students = doc.css("div.roster-cards-container")
-    name_city = students.css("div.card-text-container")
-    name_city.each do |info|
-      name = info.children.text.split[0..1].join(" ")
-      location = info.children.text.split[2..3].join(" ").delete(",")
-    binding.pry
+      doc.css("div.roster-cards-container").each do |card|
+        card.css(".student-card a").each do |info|
+        binding.pry
+        #get name and city
+        # doc.css("div.card-text-container").each do |info|
+        #   @name = info.children.text.split[0..1].join(" ")
+        #   @location = info.children.text.split[2..3].join(" ").delete(",")
+            #binding.pry
+        end
+      end
+
+        #get profile link
+        link_end = student_info.css("div.student-card").xpath('//div/a/@href').first.value
+        @profile = "./fixtures/student-site/#{link_end}"
+        binding.pry
+
+        #populate student hash, save to student array
+        student = {:name => @name, :location => @location, :profile_url => @profile}
+        student_array << student
+        #binding.pry
+
     end
-
-      [0]
-
-  end
 
   def self.scrape_profile_page(profile_url)
 
